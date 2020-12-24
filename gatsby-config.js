5af57d666be81290f3b372a5bbf5284df8bc9ff3
Thiago Villa <thiagovilla89@gmail.com>
2020-12-24 17:54:51 -0300

    Install Google Analytics Gatsby plugin and add GA tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,10 @@
 module.exports = {
     plugins: [
-
+        {
+            resolve: 'gatsby-plugin-google-gtag',
+            options: {
+                trackingIds: ['UA-101599702-3']
+            }
+        }
     ]
 }
