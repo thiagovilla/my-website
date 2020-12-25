2d6882a432e70a789e81660b86139b6fc8781e39
Thiago Villa <thiagovilla89@gmail.com>
2020-12-25 10:23:31 -0300

    Install, set up and test DatoCMS source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
-    plugins: [
-        {
-            resolve: 'gatsby-plugin-google-gtag',
-            options: {
-                trackingIds: ['UA-101599702-3']
-            }
-        }
-    ]
-}
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: ["UA-101599702-3"],
+      },
+    },
+    {
+      resolve: "gatsby-source-datocms",
+      options: {
+        apiToken: "5e93898ac811375192253b8a9362bc",
+      },
+    },
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,42 @@
-import * as React from "react"
+import * as React from "react";
+import { graphql } from "gatsby";
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: "96px",
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
   maxWidth: 320,
-}
+};
 const paragraphStyles = {
   marginBottom: 48,
-}
+};
 const listStyles = {
   marginBottom: 96,
   paddingLeft: 0,
-}
+};
 const linkStyle = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: "16px",
   verticalAlign: "5%",
-}
+};
 const docLinkStyle = {
   ...linkStyle,
   listStyleType: "none",
   marginBottom: 24,
-}
+};
 
 // markup
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
   return (
     <main style={pageStyles}>
       <title>Thiago Villa</title>
-      <h1 style={headingStyles}>
-        Thiago Villa
-      </h1>
+      <h1 style={headingStyles}>Thiago Villa</h1>
       <p style={paragraphStyles}>
         Coming soon!{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
@@ -46,16 +45,37 @@ const IndexPage = () => {
       </p>
       <ul style={listStyles}>
         <li style={docLinkStyle}>
-          Find me on <a
-            style={linkStyle}
-            href="https://linkedin.com/in/thiagovilla89"
-          >
+          Find me on{" "}
+          <a style={linkStyle} href="https://linkedin.com/in/thiagovilla89">
             LinkedIn
           </a>
         </li>
+        {data.allDatoCmsPost.edges.map(({ node: post }) => (
+          <li>
+            {post.title} {post.meta.publishedAt}
+          </li>
+        ))}
       </ul>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+  query Last5Posts {
+    allDatoCmsPost(
+      sort: { fields: [meta___publishedAt], order: DESC }
+      limit: 5
+    ) {
+      edges {
+        node {
+          meta {
+            publishedAt
+          }
+          title
+        }
+      }
+    }
+  }
+`;
